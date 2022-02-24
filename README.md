25faf726dcb7d54494aa01fc5dcb57d6d97b63cf
omerfruk <omer.fruk3547@gmail.com>
2022-02-24 23:45:33 +0300

    Create : db baglantısi ve dokumantasyonu yazıldı

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # LibraryApp
 
 Ömer faruk taşdemir
+
+
+localhost ta oluşturulacak 
+
+>user: library
+
+>database: library
+
+>user pass : library123 
+
+şeklinde db oluşturup default olarak baglantı kurabilirsiniz gerektiği takdirde 
+kendi isteğinize göre database te bulunan config dosyalarını değiştirebilirsiniz 
---
+++ database/database.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"fmt"
+	"github.com/omerfruk/LibraryApp/models"
+	"github.com/pkg/errors"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+const (
+	HOST     = "localhost"
+	DATABASE = "library"
+	USER     = "library"
+	PASSWORD = "library123"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB {
+	return db
+}
+func ConnectDB() error {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable port=5432", HOST, USER, PASSWORD, DATABASE)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		return errors.Wrapf(err, "DB'ye bağlanılamadı: "+dsn)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	sqlDB.SetMaxOpenConns(2)
+	return nil
+}
+func AutoMigrate() {
+	db.AutoMigrate(&models.Kullanici{})
+	db.AutoMigrate(&models.Katagori{})
+	db.AutoMigrate(&models.Kitap{})
+}
