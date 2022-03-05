1e114fac28bbf6a457e9a36522e8271b219333dd
omerfruk <omer.fruk3547@gmail.com>
2022-03-05 19:51:16 +0300

    New : vue Good table eklendi

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,14 @@ import {router} from "./router";
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueGoodTablePlugin from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
+Vue.use(VueGoodTablePlugin);
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
